4f898a719af791d02f201f562fe4d9410d421cba
kavinsathyan <79074982+kavinsathyan@users.noreply.github.com>
2021-04-14 09:12:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function getparagraph1()
+{
+    var inputs =[];
+    
+     for(var i = 1;i<=6;i++ )
+     {
+         inputs.push(document.getElementById("para1_input_box_" +i).value);
+         inputs.join(". ");
+     }   
+     document.getElementById("showparagraph1").innerHTML = inputs.join(". ")
+}
+
+
+function getparagraph2()
+{
+    var inputs =[];
+    
+     for(var i = 1;i<=6;i++ )
+     {
+         inputs.push(document.getElementById("para2_input_box_" +i).value);
+         inputs.join(". ");
+     }   
+     document.getElementById("showparagraph2").innerHTML = inputs.join(". ")
+}
